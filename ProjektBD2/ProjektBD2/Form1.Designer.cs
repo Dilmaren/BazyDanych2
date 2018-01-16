c0ca7136faa4dc7758c1c363b25ea7659c92db25
Dilmaren <bartosz.nedzewicz@gmail.com>
2018-01-17 00:05:02 +0100

    v1.5.1 Usunięcie zbędnych rzeczy na ekranie głównym
    Usunięto możliwość tworzenia HCO i Adresu na ekranie głównym - niepotrzebne funkcjonalności

--- ProjektBD2/ProjektBD2/Form1.Designer.cs
+++ ProjektBD2/ProjektBD2/Form1.Designer.cs
@@ -32,9 +32,7 @@
             this.hCOSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.bD2DataSet = new ProjektBD2.BD2DataSet();
             this.hCOSetTableAdapter = new ProjektBD2.BD2DataSetTableAdapters.HCOSetTableAdapter();
-            this.btn1 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.hCOSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bD2DataSet)).BeginInit();
             this.SuspendLayout();
@@ -53,16 +51,6 @@
             // 
             this.hCOSetTableAdapter.ClearBeforeFill = true;
             // 
-            // btn1
-            // 
-            this.btn1.Location = new System.Drawing.Point(12, 71);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(106, 55);
-            this.btn1.TabIndex = 1;
-            this.btn1.Text = "Create HCO";
-            this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.btn1_Click);
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
@@ -73,24 +61,12 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(24, 166);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(188, 42);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Tworzenie Adresu za pomocą EF";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1152, 434);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(504, 255);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.btn1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -104,9 +80,7 @@
         private BD2DataSet bD2DataSet;
         private System.Windows.Forms.BindingSource hCOSetBindingSource;
         private BD2DataSetTableAdapters.HCOSetTableAdapter hCOSetTableAdapter;
-        private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
